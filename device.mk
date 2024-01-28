b9c85d13c4a56599de3dcc4e0de7f2a3e54ef8dd
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-01-28 21:14:48 +0530

    Apollo: Silence some spammy logging
    HWC and cit sensor logspam is so intense it bumps up logd's idle CPU
    usage up to over 3%. With this change, logs are much cleaner and logd
    idle CPU usage is reduced to under 1%, while still allowing error logs
    to pass through.
    - Do this only in user/debug builds (except eng).
    - Add wifi and cnss too while we're at it, they can get noisy at times.

--- device.mk
+++ device.mk
@@ -224,6 +224,27 @@ PRODUCT_PACKAGES += \
 
 $(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
 
+# Logging
+SPAMMY_LOG_TAGS := \
+    MiStcImpl \
+    SDM \
+    SDM-histogram \
+    SRE \
+    WifiHAL \
+    cnss-daemon \
+    libcitsensorservice@2.0-impl \
+    libsensor-displayalgo \
+    libsensor-parseRGB \
+    libsensor-ssccalapi \
+    sensors \
+    vendor.qti.hardware.display.composer-service \
+    vendor.xiaomi.sensor.citsensorservice@2.0-service
+
+ifneq ($(TARGET_BUILD_VARIANT),eng)
+PRODUCT_VENDOR_PROPERTIES += \
+    $(foreach tag,$(SPAMMY_LOG_TAGS),log.tag.$(tag)=E)
+endif
+
 # Media configs
 PRODUCT_PACKAGES += \
     media_codecs_c2.xml \

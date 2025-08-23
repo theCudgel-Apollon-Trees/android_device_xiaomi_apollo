f51f24b39059533eea348902016eb6adc53bda94
Arian <arian.kulmer@web.de>
2025-08-23 08:28:49 +0300

    Apollo: Don't explicitly build libwfdaac_vendor
    This has been converted to blueprint and is built implicitly as
    dependency of libwfdmmsrc_proprietary.so.

--- device.mk
+++ device.mk
@@ -401,9 +401,5 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
-# Wi-Fi Display
-PRODUCT_PACKAGES += \
-    libwfdaac_vendor
-
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/apollo/apollo-vendor.mk)

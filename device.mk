2772dc8b36aca22e0186bde0776cce5d3728e396
Yumi Yukimura <me.cafebabe@gmail.com>
2025-11-24 03:08:29 +0800

    Apollo: Update some soong config variables to bool type
    Change-Id: Ice4a8e2cc6d756a743116b46299cf833a0e78c57

--- device.mk
+++ device.mk
@@ -129,7 +129,7 @@ PRODUCT_PACKAGES += \
     libmegface \
     libpiex_shim
 
-$(call soong_config_set,camera,override_format_from_reserved,true)
+$(call soong_config_set_bool,camera,override_format_from_reserved,true)
 
 # Configstore
 PRODUCT_PACKAGES += \

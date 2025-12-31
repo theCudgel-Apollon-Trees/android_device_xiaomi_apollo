d38f2af50dba38018191261525e36b44cec403f6
Dobsgw <38656887+Dobsgw@users.noreply.github.com>
2025-12-31 15:13:46 +0500

    Apollo: opt-out of dialer's incoming call proximity sensor check

--- device.mk
+++ device.mk
@@ -329,6 +329,7 @@ PRODUCT_PACKAGES += \
     CarrierConfigOverlayCommon \
     DeviceAsWebcamResCommon \
     FrameworkResOverlayCommon \
+    LineageDialerOverlayCommon \
     LineageSDKOverlayCommon \
     LineageSettingsOverlayCommon \
     SettingsOverlayCommon \

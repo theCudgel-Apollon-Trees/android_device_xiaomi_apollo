71aac61e7126546d36226ae16c55c65f8b715e32
Joel Lucas <joelcba57@gmail.com>
2026-02-06 19:46:15 +0500

    Apollo: Spoof BuildFingerprint as Pixel Beta
    * It is necessary to pass integrity with the built-in Keybox Importer, as our stock fingerprint is not "Valid"

--- lineage_apollo.mk
+++ lineage_apollo.mk
@@ -24,4 +24,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="apollo_global-user 12 RKQ1.211001.001 V14.0.4.0.SJDMIXM release-keys" \
-    BuildFingerprint=Xiaomi/apollo_global/apollo:12/RKQ1.211001.001/V14.0.4.0.SJDMIXM:user/release-keys
+    BuildFingerprint=google/husky_beta/husky:16/BP41.250822.010/14082742:user/release-keys
